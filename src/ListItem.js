50d7fe7e96d3a43cc0c06cb786d02cabf133b884
Franziska Kissling <frankissling@yahoo.co.uk>
2023-04-18 21:36:44 +0100

    fave button changes color

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,12 +1,36 @@
 import EditForm from "./EditForm";
-
+import {useState} from "react";
 export default function ListItem(jokeItems){
-    console.log("props joke in ListItem", jokeItems.jokeItem)
+    function log(){
+        console.log("start works")
+    }
+const [star, setStar]=useState(false);
+//console.log("jokeItem", jokes)
+console.log("star before function", star)
+
+function removeItem(){
+
+console.log("id", jokeItems.index)
+//const newItem = {...props={}};
+//      newItem.splice(props.index,1)
+//     console.log("removeTodo", newItem)
+//    setNewTodoList(newItem);
+//     console.log("newTodolist after remove", newTodoList)
+//     
+}
     return(
-        <>
-        <li>
-          {jokeItems.jokeItem}
-        </li>
-        </>
+     
+        // style={{color: "red"}}
+        <li className="jokeListed" id={jokeItems.index}>
+            {/* //     style={{
+        backgroundColor: props.index % 2 ? "lavender" : "hotpink",
+      }} */}
+      {jokeItems.jokeItem}
+          <button className="star" style={{color:star?"yellow":"#ccc"}}onClick={()=>{setStar(!star)}}> &#9733;</button>
+          {/* //onClick={() => setShowTodos(!showTodos)}>{showTodos ? 'HIDE' : 'SHOW'} */}
+       
+        <button className="remove"onClick={removeItem}>Remove</button>
+         </li>
+    
     )
 }
