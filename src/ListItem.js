dc2fc0156dc839e514426f3e2ed0c445c86315c6
Franziska Kissling <frankissling@yahoo.co.uk>
2023-04-20 10:52:23 +0100

    Edit Items works

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,32 +1,46 @@
 import EditForm from "./EditForm";
 import {useState} from "react";
 export default function ListItem(jokeItems){
-    console.log("jokes in listitem yy", jokeItems)
     function log(){
         //console.log("start works")
     }
 const [star, setStar]=useState(false);
-//console.log("jokeItem", jokes)
-//console.log("star before function", star)
+const [changeItem, setChangeItem]=useState(false);
+const[showEditForm, setShowEditForm]=useState(false);
 
 const[newJokeList, setNewJokeList]=useState(jokeItems.jokeList);
-console.log("newJokeList", newJokeList, [...newJokeList])
 //jokeItems.setJokes(newJokeList);
-
+let id=jokeItems.index;
+let editedJokeList=[];
 
 function removeItem(){
-    console.log("removeItem running")
-    console.log("newjokelist in removeitem", jokeItems.jokeList)
-let id=jokeItems.index;
-console.log("id", jokeItems.index)
 let newItem = [...jokeItems.jokeList];
-console.log("newItem", newItem)
 newItem.splice(id,1)
 console.log("spliced newItem", newItem)
 jokeItems.setJokes(newItem); 
-
-//newItem="";   
 }
+function handleJokeChanges(e){
+    e.preventDefault();
+    console.log("e.target.value", e.target.value)
+    let editedJoke=e.target.value;
+    let editedJokeList=[...jokeItems.jokeList]
+    let spliceId=id+1;
+    let newEditedJokeList=[editedJokeList.splice(id,0,editedJoke)];
+    console.log("edited jokelist, deditedJoke", editedJokeList, editedJoke); 
+    console.log("id in edited joke", id)
+    editedJokeList.splice(spliceId,1);
+    console.log("editedJoke after splice", editedJokeList)
+    console.log("edited joke with id", editedJokeList[id])
+    jokeItems.setJokes(editedJokeList); 
+    editedJokeList=[];
+    editedJoke="";
+}
+function handleFormSubmit(e) {
+    e.preventDefault();
+    setChangeItem(false);
+  
+    //props.addSkill(newSkill);
+  }
     return(
      
         // style={{color: "red"}}
@@ -39,7 +53,15 @@ jokeItems.setJokes(newItem);
           {/* //onClick={() => setShowTodos(!showTodos)}>{showTodos ? 'HIDE' : 'SHOW'} */}
        
         <button className="remove"onClick={removeItem}>Remove</button>
-        <button className="edit">Edit</button>
+        <button className="edit" onClick={()=>{setChangeItem(true)}}>Edit</button>
+
+      
+        <br />
+        <form className="editForm" onSubmit={handleFormSubmit} style={{display: changeItem?"inline-block":"none"}}>
+        <input className="editInput" type = "text" onChange={handleJokeChanges} value={jokeItems.jokeItem} style={{display:changeItem?"inline-block":"none", width: "850px", textSlign: "center"}}></input>
+        <br />
+        <button className="submitEdit" type="submit" style={{display:changeItem?"inline-block":"none"}}>Submit</button>
+        </form>
          </li>
     
     )
